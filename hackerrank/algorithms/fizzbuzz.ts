31db0fe2dd0dae55e43bfdeec59e64d5fafecfaa
Sinem Uyarer <sinemuyarer@mac.speedport.ip>
2025-12-07 18:42:51 +0100

    feat:fizzbuzz question

---
+++ hackerrank/algorithms/fizzbuzz.ts
@@ -0,0 +1,15 @@
+function fizzbuzz(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+console.log(fizzbuzz(15));
